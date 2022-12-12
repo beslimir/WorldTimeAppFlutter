7e3cb2fb04c7612c6ed4c5b39bb234ec8ed720c5
beslimir <marin.beslo@gmail.com>
2022-12-12 12:41:11 +0100

    Formatting time and showing after Loading Screen

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -10,14 +10,14 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  String time = 'loading';
 
   void setupWorldTime() async {
     WorldTime instance = WorldTime(location: 'Zagreb', flag: 'croatia.png', url: 'Europe/Zagreb');
     await instance.getTime();
-    print(instance.time);
-    setState(() {
-      time = instance.time;
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time
     });
   }
 
@@ -32,7 +32,7 @@ class _LoadingState extends State<Loading> {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(50.0),
-        child: Text(time)
+        child: Text('Loading')
       ),
     );
   }
